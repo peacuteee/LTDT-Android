e9809986438a855e5458cd09296f3681de5c3cfe
Trần Tài 2509 <tait04092@gmail.com>
2026-08-25 02:52:12 +0700

    Add Kotlin program to print even numbers from 1 to 20

---
+++ Kotlin-Co-Ban/Bai-Tap-Tuan-2/Bai3.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val ten: String = "Trần Thanh Tài"
+    val masv: String = "23115053122139"
+    println("Họ và tên: $ten và masv là $masv")
+    println("Bài 3: In các số chẵn từ 1 đến 20")
+    for (i in 1..20)
+    	if (i % 2 == 0)
+    		println("$i")  
+}
