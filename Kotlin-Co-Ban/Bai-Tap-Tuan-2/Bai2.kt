b0d3d8b271f081352ee12d444368d999033d14e7
Trần Tài 2509 <tait04092@gmail.com>
2026-08-25 02:50:23 +0700

    Add main function to calculate sum from 1 to 100

---
+++ Kotlin-Co-Ban/Bai-Tap-Tuan-2/Bai2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val ten: String = "Trần Thanh Tài"
+    val masv: String = "23115053122139"
+    println("Họ và tên: $ten và masv là $masv")
+    println("Bài 1: Tính tổng các số từ 1 đến 100")
+    var tong: Int = 0
+    for (i in 1..100)
+    	tong = tong + i
+    	println("Tổng là: $tong")  
+}
