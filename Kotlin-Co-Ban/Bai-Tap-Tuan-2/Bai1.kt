49b3dd95281365a72b64e21a395b0883a7050bc1
Trần Tài 2509 <tait04092@gmail.com>
2026-08-25 02:46:49 +0700

    Add main function to print name and numbers 1 to 10

---
+++ Kotlin-Co-Ban/Bai-Tap-Tuan-2/Bai1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val ten: String = "Trần Thanh Tài"
+    val masv: String = "23115053122139"
+    println("Họ và tên: $ten và masv là $masv")
+    println("Bài 1: in các số từ 1 đến 10")
+    for (i in 1..10)
+    	println("$i")  
+}
